1c90497707568700167cb3b1929330cb1d460df5
winlin <winlin@vip.126.com>
2021-03-17 13:53:55 +0800

    Threads: Use coroutine to consume recv/srtp packets.

--- trunk/src/app/srs_app_hybrid.cpp
+++ trunk/src/app/srs_app_hybrid.cpp
@@ -183,6 +183,16 @@ srs_error_t SrsHybridServer::initialize()
     // A monitor to check the clock wall deviation, per clock tick.
     timer_->subscribe(20 * SRS_UTIME_MILLISECONDS, clock_monitor_);
 
+    // Consume the async cooked SRTP packets.
+    if ((err = _srs_async_srtp->consume()) != srs_success) {
+        return srs_error_wrap(err, "srtp");
+    }
+
+    // Consume the async received UDP packets.
+    if ((err = _srs_async_recv->consume()) != srs_success) {
+        return srs_error_wrap(err, "recv");
+    }
+
     vector<ISrsHybridServer*>::iterator it;
     for (it = servers.begin(); it != servers.end(); ++it) {
         ISrsHybridServer* server = *it;
