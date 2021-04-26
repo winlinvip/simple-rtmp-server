bluebox514/srs

Threads-Hybrid: Support communicate between threads by chan and slot
1. Hybrid thread is responder, API thread is initiator.
2. Responder read message from initiator-slot, write message to responder-slot.
3. Initiator write message to initiator-slot, read message from responder-slot.
4. Responder start a coroutine to consume requests and response it.

trunk/src/app/srs_app_hybrid.hpp
@@ -29,6 +29,7 @@
 #include <vector>
 
 #include <srs_app_hourglass.hpp>
+#include <srs_app_threads.hpp>
 
 class SrsServer;
 class SrsServerAdapter;
@@ -49,9 +50,8 @@ public:
 };
 
 // The hybrid server manager.
-class SrsHybridServer : public ISrsHourGlass
+class SrsHybridServer : public ISrsHourGlass, public ISrsThreadResponder
 {
-    friend class SrsApiServer;
 private:
     std::vector<ISrsHybridServer*> servers;
     SrsHourGlass* timer_;
@@ -68,6 +68,8 @@ public:
 private:
     virtual srs_error_t setup_ticks();
     virtual srs_error_t notify(int event, srs_utime_t interval, srs_utime_t tick);
+private:
+    srs_error_t on_thread_message(SrsThreadMessage* msg, SrsThreadPipeChannel* channel);
 };
 
 extern SrsHybridServer* _srs_hybrid;

winlinvip/simple-rtmp-server

trunk/src/app/srs_app_hybrid.hpp
@@ -29,6 +29,7 @@
 #include <vector>
 
 #include <srs_app_hourglass.hpp>
+#include <srs_app_threads.hpp>
 
 class SrsServer;
 class SrsServerAdapter;
@@ -49,9 +50,8 @@ public:
 };
 
 // The hybrid server manager.
-class SrsHybridServer : public ISrsFastTimer
+class SrsHybridServer : public ISrsFastTimer, public ISrsThreadResponder
 {
-    friend class SrsApiServer;
 private:
     std::vector<ISrsHybridServer*> servers;
     SrsFastTimer* timer_;
@@ -70,6 +70,8 @@ public:
 // interface ISrsFastTimer
 private:
     srs_error_t on_timer(srs_utime_t interval, srs_utime_t tick);
+private:
+    srs_error_t on_thread_message(SrsThreadMessage* msg, SrsThreadPipeChannel* channel);
 };
 
 extern SrsHybridServer* _srs_hybrid;